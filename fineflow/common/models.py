e90315427c230a3a3f1e3bff98b6faf0c8ea6e99
stuandrews1618 <42622036+stuandrews1618@users.noreply.github.com>
2019-11-02 19:34:04 +0000

    testcommit

--- fineflow/common/models.py
+++ fineflow/common/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class CompanyType(models.Model):
+    company_type = models.Charfield('Type of max_length=100)
+
+
+
 # Create your models here.
--- fineflow/companies/models.py
+++ fineflow/companies/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from contacts.models import Contact
 
 # Create your models here.
 
-class Company:
-    company_name = models.TextField(max_length=100)
-    website = models.
+class Company(models.Model):
+    company_name = models.TextField(max_length=255)
+    website = models.URLfield(max_length=255)
+    assigned_to = models.Foreignkey(User, on_delete=models.DO_NOTHING)
     qfl_relationship = models.TextField(man_length = 100)
         def __str__(self):
         return self.first_name + " " + self.last_name   
